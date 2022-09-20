9b620f0b267bab2330eccba43b79f6b5e5ced459
rohit <you@example.com>
2022-09-20 17:15:27 +0530

    add

--- after.sh
+++ after.sh
@@ -2,5 +2,12 @@
 
 sudo docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 351552566849.dkr.ecr.ap-south-1.amazonaws.com
 
+sudo docker container prune --filter="label==website.name=sample"
+
+sudo docker rmi 351552566849.dkr.ecr.ap-south-1.amazonaws.com/code-pipeline-repo:latest
+
 sudo docker pull 351552566849.dkr.ecr.ap-south-1.amazonaws.com/code-pipeline-repo:latest
 
+sudo docker run -itd -p 8080:80 351552566849.dkr.ecr.ap-south-1.amazonaws.com/code-pipeline-repo:latest
+
+sudo docker ps
