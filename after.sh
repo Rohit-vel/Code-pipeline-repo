7b9cd4ebffba9a4869599e36581a3d339a58bc3a
rohit <you@example.com>
2022-09-20 15:56:42 +0530

    ss

--- after.sh
+++ after.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 351552566849.dkr.ecr.ap-south-1.amazonaws.com
+sudo docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 351552566849.dkr.ecr.ap-south-1.amazonaws.com
 
-docker pull 351552566849.dkr.ecr.ap-south-1.amazonaws.com/code-pipeline-repo:latest
+sudo docker pull 351552566849.dkr.ecr.ap-south-1.amazonaws.com/code-pipeline-repo:latest
 
