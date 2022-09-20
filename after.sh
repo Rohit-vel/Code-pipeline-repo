3d070f283d3ba3b9ef2279ea2a6ada8635a2140e
neo-user <neo.123@gmail.com>
2022-09-20 06:53:14 +0000

    2nd update

--- after.sh
+++ after.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 351552566849.dkr.ecr.ap-south-1.amazonaws.com
+
+docker pull 351552566849.dkr.ecr.ap-south-1.amazonaws.com/code-pipeline-repo:latest
+
